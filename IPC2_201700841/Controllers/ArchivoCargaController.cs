e8c877ee38d473e58c0caf6c240088a6e8573883
LuisCutzal <2842258270404@ingenieria.usac.edu.gt>
2020-09-11 12:39:11 -0600

    base de datos actualizada
    se puede enviar informacion a la base de datos

--- IPC2_201700841/Controllers/ArchivoCargaController.cs
+++ IPC2_201700841/Controllers/ArchivoCargaController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Win32;
+﻿using IPC2_201700841.Models;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -13,37 +15,68 @@ namespace IPC2_201700841.Controllers
 {
     public class ArchivoCargaController : Controller
     {
-        
+        string connectionString = @"Data Source=localhost\SQLEXPRESS;initial Catalog=FaseIpc2_201700841;Integrated Security=True;MultipleActiveResultSets=True;App=EntityFramework";
         // GET: FileUpload
         public ActionResult Index()
         {
-            //var items = GetFiles();
-            //return View(items);
-            
+                       
             return View();
         }
-        
         [HttpPost]
-        
-        public ActionResult Index(HttpPostedFileBase file) 
+        public ActionResult Index(HttpPostedFileBase file)
         {
-
             XmlReader reader = XmlReader.Create(new StreamReader(file.InputStream));
-            //List<string> datos = new List<string>();
-            string datos = "";
-            while (reader.Read()) 
+            using (SqlConnection sql1 = new SqlConnection(connectionString)) 
             {
-                if (reader.IsStartElement()) 
+                sql1.Open();
+                string prueba = " ";
+                while (reader.Read()) 
                 {
-                    switch (reader.Name.ToString())
+                    if (reader.IsStartElement()) 
                     {
-                        case "ficha":
-                            datos += reader.ReadString();
-                            break;
+                        switch (reader.Name.ToString()) 
+                        {
+                            case "ficha":
+                                prueba += "insert into Archivo values(";
+                                break;
+                            case "color":
+                                prueba += "'" + reader.ReadString() + "'" + ",";
+                                break;
+                            case "columna":
+                                prueba += "'" + reader.ReadString() + "'" + ",";
+                                break;
+                            case "fila":
+                                prueba += reader.ReadString() + ");";
+                                if (prueba != "") 
+                                {
+                                    SqlCommand sql2 = new SqlCommand(prueba, sql1);
+                                    sql2.ExecuteNonQuery();
+                                    prueba = "";
+                                }
+                                break;
+                        }
                     }
                 }
+                sql1.Close();
             }
             return View("Index");
         }
+        //[HttpGet]
+        //public FileResult Generar()
+        //{
+        //    using (FaseIpc2_201700841Entities db = new FaseIpc2_201700841Entities())
+        //    {
+
+        //    }
+        //}
+        public ActionResult Tab()
+        {
+            using (FaseIpc2_201700841Entities db = new FaseIpc2_201700841Entities())
+            {
+                return View(db.Archivo.ToList());
+            }
+
+        }
+
     }
 }
