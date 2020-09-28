1e6b44ae1aa41637a8a7481d2e1a982eec63310c
LuisCutzal <2842258270404@ingenieria.usac.edu.gt>
2020-09-27 22:31:08 -0600

    creando de nuevo el proyecto
    se esta creando de nuevo todo el proyecto, para fines esteticos, entre otros

---
+++ Proyect/IPC2_201700841/IPC2_201700841/Controllers/AdministradorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IPC2_201700841.Controllers
+{
+    public class AdministradorController : Controller
+    {
+        // GET: Administrador
+        public ActionResult Admin()
+        {
+            return View();
+        }
+    }
+}
