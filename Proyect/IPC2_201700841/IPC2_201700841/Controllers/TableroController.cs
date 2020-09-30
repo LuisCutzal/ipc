e3e0b3dbcf314109da955471b164af55d57c7993
LuisCutzal <2842258270404@ingenieria.usac.edu.gt>
2020-09-29 22:41:13 -0600

    prueba
    no se porque, pero se eliminaron varias cosas dentro de la carpeta, ahora ya las volvi a colocar, estoy como en el commit anterior

--- Proyect/IPC2_201700841/IPC2_201700841/Controllers/TableroController.cs
+++ Proyect/IPC2_201700841/IPC2_201700841/Controllers/TableroController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using IPC2_201700841.Models.ViewModels;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
+using System.Xml;
+using System.Data.SqlClient;
 
 namespace IPC2_201700841.Controllers
 {
@@ -13,8 +17,57 @@ namespace IPC2_201700841.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult TableroSolitario (ArchivoModel file)
+        {
+            string ruta = Server.MapPath("~/");//raiz del proyecto
+            string RutaArchivo = Path.Combine(ruta + "/Archivo/ejemplo.xml");
+            if (!ModelState.IsValid)//si el modelo que estoy pasando es valido
+            {
+                return View("Index", file); //model invalido
+            }
+            file.Archivo.SaveAs(RutaArchivo);
+            XmlReader reader = XmlReader.Create(RutaArchivo);
 
-        public ActionResult TableroVersus()
+            /*este codigo es para insertar en la bd, 
+            no se usara ese connectionstring porque no usare la bd*/
+            /*using (SqlConnection sql1 = new SqlConnection(connectionString)) 
+            {
+                sql1.Open();
+                string prueba = " ";
+                while (reader.Read())
+                {
+                    if (reader.IsStartElement())
+                    {
+                        switch (reader.Name.ToString())
+                        {
+                            case "ficha":
+                                prueba += "insert into Archivo values(";
+                                break;
+                            case "color":
+                                prueba += "'" + reader.ReadString() + "'" + ",";
+                                break;
+                            case "columna":
+                                prueba += "'" + reader.ReadString() + "'" + ",";
+                                break;
+                            case "fila":
+                                prueba += reader.ReadString() + ");";
+                                if (prueba != "")
+                                {
+                                    SqlCommand sql2 = new SqlCommand(prueba, sql1);
+                                    sql2.ExecuteNonQuery();
+                                    prueba = "";
+                                }
+                                break;
+                        }
+                    }
+                }
+                sql1.Close();
+            }*/
+            return View("TableroSolitario");
+        }
+
+        /*public ActionResult TableroVersus()
         {
             return View();
         }
@@ -23,5 +76,7 @@ namespace IPC2_201700841.Controllers
         {
             return View();
         }
+        
+         esto es para los tableros de los otrso 2 modos*/
     }
 }
