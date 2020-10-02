e35ed74e705408019c8db78251c9932dcd7230f6
LuisCutzal <2842258270404@ingenieria.usac.edu.gt>
2020-10-01 20:59:59 -0600

    funciona cargar, guardar y leer xml
    ya funcionan los metodos de carga, lectura y escritura de los archivos xml al tablero, ahora me hace falta agregar fichas de uno en uno y por turnos (no en carga masiva)

--- Proyect/IPC2_201700841/IPC2_201700841/Controllers/TableroController.cs
+++ Proyect/IPC2_201700841/IPC2_201700841/Controllers/TableroController.cs
@@ -24,72 +24,7 @@ namespace IPC2_201700841.Controllers
 
         public ActionResult TableroSolitario()
         {
-            /*ObtenerContenidoA i1 = new ObtenerContenidoA();
-            i1.color = "blanco";
-            i1.fila = 4;
-            i1.columna = "D";
-            ObtenerContenidoA i2 = new ObtenerContenidoA();
-            i2.color = "negro";
-            i2.fila = 4;
-            i2.columna = "E";
-            ObtenerContenidoA i3 = new ObtenerContenidoA();
-            i3.color = "negro";
-            i3.fila = 5;
-            i3.columna = "D";
-            ObtenerContenidoA i4 = new ObtenerContenidoA();
-            i4.color = "blanco";
-            i4.fila = 5;
-            i4.columna = "E";
-            List<ObtenerContenidoA> fichas = new List<ObtenerContenidoA>();
-            fichas.Add(i1);
-            fichas.Add(i2);
-            fichas.Add(i3);
-            fichas.Add(i4);
-            Session["juego"] = fichas;*/
-            return View("~/Views/Tablero/TableroSolitario.cshtml",(List<ObtenerContenidoA>)Session["juego"]);
-            
-        }
-
-        [HttpPost]
-        public ActionResult Index (ArchivoModel file)
-        {
-            string ruta = Server.MapPath("~/");//raiz del proyecto
-            string RutaArchivo = Path.Combine(ruta + "/Archivos/ejemplo.xml");
-            //string RutaArchivo = Path.Combine(ruta + "/Archivos/" + file + ".xml");
-            if (!ModelState.IsValid)//si el modelo que estoy pasando es valido
-            {
-                return View("Index", file); //model invalido
-            }
-            file.Archivo.SaveAs(RutaArchivo);
-            XmlReader reader = XmlReader.Create(RutaArchivo);
-            
-            List<ObtenerContenidoA> prueba = new List<ObtenerContenidoA>();
-            Session["juego"] = prueba;
-            ObtenerContenidoA ficha = new ObtenerContenidoA();
-            while (reader.Read()) 
-            {
-                if (reader.IsStartElement()) {
-                    switch (reader.Name.ToString()) {
-                        case "color":
-                            ficha.color = reader.ReadString();
-                            System.Diagnostics.Debug.WriteLine(ficha.color);
-                            break;
-                        case "columna":
-                            ficha.columna = reader.ReadString();
-                            System.Diagnostics.Debug.WriteLine(ficha.columna   );
-                            break;
-                        case "fila":
-                            ficha.fila = Int32.Parse(reader.ReadString());
-                            System.Diagnostics.Debug.WriteLine(ficha.fila);
-                            prueba.Add(ficha);
-                            ficha = new ObtenerContenidoA();
-                            System.Diagnostics.Debug.WriteLine(prueba);
-                            break;
-                    }
-                }
-            }
-            reader.Close();
-            /*if (Session["juego"]==null)
+            if (Session["juego"] == null)
             {
                 ObtenerContenidoA i1 = new ObtenerContenidoA();
                 i1.color = "blanco";
@@ -114,46 +49,100 @@ namespace IPC2_201700841.Controllers
                 fichas.Add(i4);
                 Session["juego"] = fichas;
                 return View("~/Views/Tablero/TableroSolitario.cshtml", (List<ObtenerContenidoA>)Session["juego"]);
-            }*/
-
-
-            return View("Index");
+            }
+            else
+            {
+                return View("~/Views/Tablero/TableroSolitario.cshtml", (List<ObtenerContenidoA>)Session["juego"]);
+            }
         }
 
-        /*[HttpGet]
+        [HttpPost]
+        public ActionResult Index (ArchivoModel file)
+        {
+            string ruta = Server.MapPath("~/");//raiz del proyecto
+            string RutaArchivo = Path.Combine(ruta + "/Archivos/ejemplo.xml");
+            //string RutaArchivo = Path.Combine(ruta + "/Archivos/" + file + ".xml");
+            if (!ModelState.IsValid)//si el modelo que estoy pasando es valido
+            {
+                return View("Index", file); //model invalido
+            }
+            file.Archivo.SaveAs(RutaArchivo);
+            XmlReader reader = XmlReader.Create(RutaArchivo);            
+            List<ObtenerContenidoA> prueba = new List<ObtenerContenidoA>();
+            Session["juego"] = prueba;
+            if(RutaArchivo != null)
+            {
+                ObtenerContenidoA ficha = new ObtenerContenidoA();
+                while (reader.Read())
+                {
+                    if (reader.IsStartElement())
+                    {
+                        switch (reader.Name.ToString())
+                        {
+                            case "color":
+                                ficha.color = reader.ReadString();
+                                //System.Diagnostics.Debug.WriteLine(ficha.color);
+                                break;
+                            case "columna":
+                                ficha.columna = reader.ReadString();
+                                //System.Diagnostics.Debug.WriteLine(ficha.columna);
+                                break;
+                            case "fila":
+                                ficha.fila = Int32.Parse(reader.ReadString());
+                                //System.Diagnostics.Debug.WriteLine(ficha.fila);
+                                prueba.Add(ficha);
+                                ficha = new ObtenerContenidoA();
+                                //System.Diagnostics.Debug.WriteLine(prueba);
+                                break;
+                        }
+                        
+                    }
+                }
+                reader.Close();
+            }
+            //ViewBag.juego = Session["juego"] as List<ObtenerContenidoA>();
+           
+            return View("Index");
+        }
+        [HttpGet]
         public FileResult CrearArchivo()
         {
-            using (FaseIpc2_201700841Entities db = new FaseIpc2_201700841Entities())
+            //List<ObtenerContenidoA> dato = new List<ObtenerContenidoA>();
+            //Session["juego"] = dato;
+            int cont = 0;
+            var estar = new MemoryStream();
+            XmlWriterSettings config = new XmlWriterSettings();
+            config.Indent = true;
+            XmlWriter escribirxmml = XmlWriter.Create(estar, config);
+            escribirxmml.WriteStartDocument();
+            escribirxmml.WriteStartElement("tablero");
+            //System.Diagnostics.Debug.WriteLine("entro");
+            //System.Diagnostics.Debug.WriteLine(dato);
+            foreach (var busca in Session["juego"] as List<ObtenerContenidoA>)
             {
-                var estar = new MemoryStream();
-                XmlWriterSettings config = new XmlWriterSettings();
-                config.Indent = true;
-                XmlWriter escribirxmml = XmlWriter.Create(estar, config);
-                escribirxmml.WriteStartDocument();
-                escribirxmml.WriteStartElement("tablero");
-                foreach (var busca in db.Archivo)
-                {
-                    escribirxmml.WriteStartElement("ficha");
-                    escribirxmml.WriteStartElement("color");
-                    escribirxmml.WriteString(busca.color);
-                    escribirxmml.WriteEndElement();
-                    escribirxmml.WriteStartElement("columna");
-                    escribirxmml.WriteString(busca.columna);
-                    escribirxmml.WriteEndElement();
-                    escribirxmml.WriteStartElement("fila");
-                    escribirxmml.WriteString(busca.fila.ToString());
-                    escribirxmml.WriteEndElement();
-                    escribirxmml.WriteEndElement();
-                }
+                //System.Diagnostics.Debug.WriteLine("alfin");
+                escribirxmml.WriteStartElement("ficha");
+                escribirxmml.WriteStartElement("color");
+                escribirxmml.WriteString(busca.color);
+                //System.Diagnostics.Debug.WriteLine(busca.color);
                 escribirxmml.WriteEndElement();
-                escribirxmml.WriteEndDocument();
-                escribirxmml.Close();
-                estar.Position = 0;
-                var ArchivoResultado = File(estar, "application/octet-stream", " ");
-                return ArchivoResultado;
-
+                escribirxmml.WriteStartElement("columna");
+                escribirxmml.WriteString(busca.columna);
+                escribirxmml.WriteEndElement();
+                escribirxmml.WriteStartElement("fila");
+                escribirxmml.WriteString(busca.fila.ToString());
+                escribirxmml.WriteEndElement();
+                escribirxmml.WriteEndElement();
+                cont++;
             }
-        }*/
+            //System.Diagnostics.Debug.WriteLine("salio");
+            escribirxmml.WriteEndElement();
+            escribirxmml.WriteEndDocument();
+            escribirxmml.Close();
+            estar.Position = 0;
+            var ArchivoResultado = File(estar, "application/octet-stream", " ");
+            return ArchivoResultado;
+        }
 
         /*public ActionResult TableroVersus()
         {
