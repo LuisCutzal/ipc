9bcd7fc88adc7f7561e4497390f9be39ba44d0f8
LuisCutzal <2842258270404@ingenieria.usac.edu.gt>
2020-10-05 17:03:14 -0600

    turnos
    ya puedo hacer varios turnos en el tablero, aun no tengo nada de validaciones o reglas dentro del mismo

--- Proyect/IPC2_201700841/IPC2_201700841/Controllers/TableroController.cs
+++ Proyect/IPC2_201700841/IPC2_201700841/Controllers/TableroController.cs
@@ -11,6 +11,7 @@ using System.Web.WebSockets;
 using IPC2_201700841.Models;
 using Microsoft.Ajax.Utilities;
 using System.Security.Cryptography.X509Certificates;
+using System.Security.Policy;
 
 namespace IPC2_201700841.Controllers
 {
@@ -18,15 +19,11 @@ namespace IPC2_201700841.Controllers
     //[HttpPost] //enviar informacion hacia una pagina web, cualquier tipo de informacion
     public class TableroController : Controller
     {
-        
-
         // GET: Tablero
-       
         public ActionResult Index()
         {
             return View();
         }
-
         public ActionResult TableroSolitario()
         {
             if (Session["juego"] == null)
@@ -62,6 +59,7 @@ namespace IPC2_201700841.Controllers
                     if (valor == 0) //fichas negras (computadora)
                     {
                         Session["turno"] = valor;
+                        
                         /*si el valor = 0 entonces en la variable Session["turno"] coloco un valor
                         con el fin de que no sea nulo esta variable para poder diferenciarla 
                         de la variable nula la cual es para la ficha blanca*/
@@ -69,6 +67,7 @@ namespace IPC2_201700841.Controllers
                     else //fichas blancas (usuario)
                     {
                         Session["turno"] = null;
+                        
                         /*si el valor = 1 entonces en la variable Session["turno"] la creo nula
                          con el fin de que se pueda diferenciar para el color negro*/
                     }
@@ -93,12 +92,13 @@ namespace IPC2_201700841.Controllers
             }
         }
         [HttpPost]
-        public ActionResult inicio() 
+        public ActionResult InicioSolitario() //esto es para cuando estamos en el modo solitario
         {
             try
             {
                 if (Session["turno"] != null) //fichas negras (computadora)
                 {
+                    //ViewBag.Mensaje = "Turno computadora";
                     /*
                     System.Diagnostics.Debug.WriteLine("funciono cero");
                     var Nfila = fila;
@@ -122,6 +122,7 @@ namespace IPC2_201700841.Controllers
                     fichas.Add(turno1);
 
                 }
+                else { ViewBag.Mensaje = "Turno Usuario"; }
             }
             catch (Exception)
             {
@@ -130,10 +131,104 @@ namespace IPC2_201700841.Controllers
             }
             return View("~/Views/Tablero/TableroSolitario.cshtml", (List<ObtenerContenidoA>)Session["juego"]);
         }
-        
+        public ActionResult TableroVersus()
+        {
+            if (Session["juego"] == null)
+            {
+                ObtenerContenidoA i1 = new ObtenerContenidoA();
+                i1.color = "blanco";
+                i1.fila = 4;
+                i1.columna = "D";
+                ObtenerContenidoA i2 = new ObtenerContenidoA();
+                i2.color = "negro";
+                i2.fila = 4;
+                i2.columna = "E";
+                ObtenerContenidoA i3 = new ObtenerContenidoA();
+                i3.color = "negro";
+                i3.fila = 5;
+                i3.columna = "D";
+                ObtenerContenidoA i4 = new ObtenerContenidoA();
+                i4.color = "blanco";
+                i4.fila = 5;
+                i4.columna = "E";
+                List<ObtenerContenidoA> fichas = new List<ObtenerContenidoA>();
+                fichas.Add(i1);
+                fichas.Add(i2);
+                fichas.Add(i3);
+                fichas.Add(i4);
+                Session["juego"] = fichas;
+                var send = Environment.TickCount;
+                var random = new Random(send);
+                for (int i = 0; i < 1; i++)
+                {
+                    var valor = random.Next(0, 2); //valor random entre 0 y 1
+                    System.Diagnostics.Debug.WriteLine(valor);
+                    if (valor == 0) //fichas negras (jugador 1)
+                    {
+                        Session["turno"] = valor;
+                        ViewBag.Mensaje = "Turno fichas negras";
+                        /*si el valor = 0 entonces en la variable Session["turno"] coloco un valor
+                        con el fin de que no sea nulo esta variable para poder diferenciarla 
+                        de la variable nula la cual es para la ficha blanca*/
+
+                    }
+                    else //fichas blancas (usuario)
+                    {
+                        Session["turno"] = null;
+                        ViewBag.Mensaje = "Turno fichas blancas";
+                        /*si el valor = 1 entonces en la variable Session["turno"] la creo nula
+                         con el fin de que se pueda diferenciar para el color negro*/
+
+                    }
+                }
+                return View("~/Views/Tablero/TableroVersus.cshtml", (List<ObtenerContenidoA>)Session["juego"]);
+            }
+            else
+            {
+                return View("~/Views/Tablero/TableroVersus.cshtml", (List<ObtenerContenidoA>)Session["juego"]);
+            }
+        }
         [HttpPost]
-        public ActionResult Dato(int fila, string columna) 
-        { //tengo que hacerlo para un juego en solitario
+        public ActionResult InicioVersus(int fila,string columna) 
+        {
+            try
+            {
+                if (Session["turno"] != null)
+                {
+                    var NfilaV = fila;
+                    var NcolumnaV = columna;
+                    ObtenerContenidoA f1 = new ObtenerContenidoA();
+                    f1.color = "negro";
+                    f1.fila = NfilaV;
+                    f1.columna = NcolumnaV;
+                    List<ObtenerContenidoA> ficha = (List<ObtenerContenidoA>)Session["juego"];
+                    ficha.Add(f1);
+                    Session["turno"] = null;
+                    
+                }
+                else 
+                {
+                    var NfilaV = fila;
+                    var NcolumnaV = columna;
+                    ObtenerContenidoA f1 = new ObtenerContenidoA();
+                    f1.color = "blanco";
+                    f1.fila = NfilaV;
+                    f1.columna = NcolumnaV;
+                    List<ObtenerContenidoA> ficha = (List<ObtenerContenidoA>)Session["juego"];
+                    ficha.Add(f1);
+                    int valor = 1;
+                    Session["turno"] = valor;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return View("~/Views/Tablero/TableroVersus.cshtml", (List<ObtenerContenidoA>)Session["juego"]);
+        }
+        [HttpPost]
+        public ActionResult Dato(int fila, string columna) //esto es para el tablero solitario (computadora vs usuario)
+        { 
             try
             {
                 /*coloco if para saber si la variable Session["turno"] == null
@@ -274,72 +369,7 @@ namespace IPC2_201700841.Controllers
             return ArchivoResultado;
         }
         //termina no tocar nada
-        public ActionResult TableroVersus()
-        {
 
-            if (Session["juego"] == null)
-            {
-                ObtenerContenidoA i1 = new ObtenerContenidoA();
-                i1.color = "blanco";
-                i1.fila = 4;
-                i1.columna = "D";
-                ObtenerContenidoA i2 = new ObtenerContenidoA();
-                i2.color = "negro";
-                i2.fila = 4;
-                i2.columna = "E";
-                ObtenerContenidoA i3 = new ObtenerContenidoA();
-                i3.color = "negro";
-                i3.fila = 5;
-                i3.columna = "D";
-                ObtenerContenidoA i4 = new ObtenerContenidoA();
-                i4.color = "blanco";
-                i4.fila = 5;
-                i4.columna = "E";
-                List<ObtenerContenidoA> fichas = new List<ObtenerContenidoA>();
-                fichas.Add(i1);
-                fichas.Add(i2);
-                fichas.Add(i3);
-                fichas.Add(i4);
-                Session["juego"] = fichas;
-                var send = Environment.TickCount;
-                var random = new Random(send);
-                for (int i = 0; i < 1; i++)
-                {
-                    var valor = random.Next(0, 2); //valor random entre 0 y 1
-                    System.Diagnostics.Debug.WriteLine(valor);
-                    if (valor == 0) //fichas negras (computadora)
-                    {
-                        Session["turno"] = valor;
-                        /*si el valor = 0 entonces en la variable Session["turno"] coloco un valor
-                        con el fin de que no sea nulo esta variable para poder diferenciarla 
-                        de la variable nula la cual es para la ficha blanca*/
-                    }
-                    else //fichas blancas (usuario)
-                    {
-                        Session["turno"] = null;
-                        /*si el valor = 1 entonces en la variable Session["turno"] la creo nula
-                         con el fin de que se pueda diferenciar para el color negro*/
-                    }
-
-                    //if (Session["turno"] != null)
-                    //{
-                    //    var prueba = true;
-                    //    ViewData["valor"] = prueba;
-                    //}
-                    //else
-                    //{
-                    //    var prueba = false;
-                    //    ViewData["valor"] = prueba;
-                    //}
-
-                }
-                return View("~/Views/Tablero/TableroVersus.cshtml", (List<ObtenerContenidoA>)Session["juego"]);
-            }
-            else
-            {
-                return View("~/Views/Tablero/TableroVersus.cshtml", (List<ObtenerContenidoA>)Session["juego"]);
-            }
-        }
         /*
         public ActionResult TableroTorneo()
         {
